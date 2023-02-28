7b4da7e3c400d2da1c8e00003c6f1852b86d0542
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-02-27 19:59:28 -0500

    posting data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Nav from './auth/Nav'
 import { Roboto } from '@next/font/google'
+import QueryWrapper from './auth/QueryWrapper'
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -21,8 +22,11 @@ export default function RootLayout({
       */}
       <head />
       <body className={`mx-4 md:mx-48 xl:mx-96 ${roboto.variable} bg-gray-200`}>
-        <Nav />
-        {children}</body>
+        <QueryWrapper>
+          <Nav />
+          {children}
+        </QueryWrapper>
+      </body>
     </html>
   )
 }
