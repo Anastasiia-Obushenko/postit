9400dcbd3e57b16a0f1277da1214c2ac7b5c2d0a
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-02-28 19:35:20 -0500

    get data from database

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
 'use client'
 
+import axios from 'axios'
 import AddPost from './components/AddPost'
+import { useQuery } from '@tanstack/react-query'
+
+// Fetching all posts
+const allPosts = async () => {
+  const response = await axios.get('/api/posts/getPost')
+  return response.data
+}
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  })
+  if (error) return error
+  if (isLoading) return 'Loading...'
   return (
     <main >
       <AddPost />
